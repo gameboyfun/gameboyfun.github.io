d4ad2394e4597a87f0cd4e046da35006e3c84958
gameboyfun <gameboyfun@hotmail.com>
2023-06-25 22:17:49 +0700

    :rocket: : add data

--- pages/index.vue
+++ pages/index.vue
@@ -79,7 +79,36 @@
 
 <script>
 export default {
-  name: 'IndexPage',
+  data() {
+    return {
+      fullName: "Your Full Name",
+      jobTitle: "Your Job Title",
+      aboutMe: "Write a brief description about yourself here.",
+      experiences: [
+        {
+          position: "Job Position",
+          company: "Company Name",
+          startDate: "Start Date",
+          endDate: "End Date",
+          responsibilities: [
+            "Responsibility 1",
+            "Responsibility 2",
+            "Responsibility 3"
+          ]
+        }
+      ],
+      educationList: [
+        {
+          degree: "Degree",
+          institution: "Institution Name",
+          startDate: "Start Date",
+          endDate: "End Date"
+        }
+      ],
+      email: "yourname@example.com",
+      phone: "123-456-7890"
+    };
+  }
 }
 </script>
 
